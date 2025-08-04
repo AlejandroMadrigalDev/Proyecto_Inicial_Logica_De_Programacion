10a793341742bd568d1ee456638856139a756b35
AlejandroMadrigalDev <alejandromadrigal_1999@outlook.com>
2025-08-03 22:06:27 -0500

    Subiendo Carpeta

---
+++ app.js
@@ -0,0 +1,10 @@
+let numeroSecreto = 43;
+let numeroUsuario = prompt("Me indicas un numero por favor: ");
+
+console.log(numeroUsuario);
+
+if (numeroUsuario == numeroSecreto) {
+    alert("Has acertado");
+}
+
+// alert("Hola Mundo"); Muestra cuadro de dialogo con el mensaje que se encuentra entre comillas.
