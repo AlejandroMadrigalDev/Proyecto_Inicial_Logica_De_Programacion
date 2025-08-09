5e48660e379d86d58d6d76c7e60b594b653ad890
AlejandroMadrigalDev <alejandromadrigal_1999@outlook.com>
2025-08-08 23:45:39 -0500

    Else

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 let numeroSecreto = 43;
-let numeroUsuario = prompt("Me indicas un numero por favor: ");
+let numeroUsuario = prompt("Me indicas un numero entre 1 y 20 por favor: ");
 
 console.log(numeroUsuario);
 
 if (numeroUsuario == numeroSecreto) {
     alert("Has acertado");
+} else {
+    alert("Lo siento, no acertaste el numero.")
 }
 
 // alert("Hola Mundo"); Muestra cuadro de dialogo con el mensaje que se encuentra entre comillas.
+
+/*
+    Diversos tipos de comentarios
+*/
