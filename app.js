838f08962739e6b99bf2bd3daa1e9690350d7011
AlejandroMadrigalDev <alejandromadrigal_1999@outlook.com>
2025-08-10 21:23:18 -0500

    Curso completo

--- app.js
+++ app.js
@@ -1,27 +1,34 @@
-let numeroSecreto = 11;
+let cantidadMaxima = 100;
+let numeroSecreto = Math.floor(Math.random()*cantidadMaxima)+1; // Genera un numero aleatorio entre 1 y 100
 let numeroUsuario = 0;
 let intentos = 1;
-let intentosPalabra = "intento"
+//let intentosPalabra = "intento"
+let intentosPermitidos = 5;
 
 while (numeroUsuario != numeroSecreto) {
-    numeroUsuario = prompt("Me indicas un numero entre 1 y 20 por favor: ");
+    numeroUsuario = parseInt(prompt(`Me indicas un numero entre 1 y ${cantidadMaxima} por favor: `));
 
-    console.log(numeroUsuario);
+    if (intentos == intentosPermitidos) {
+        alert(`Lo siento, no acertaste el numero, ya se te acabaron los ${intentosPermitidos} intentos.`);
+        break;
+    }
+    console.log(typeof(numeroUsuario)); // Muestra el tipo de dato de la variable numeroUsuario
 
     if (numeroUsuario == numeroSecreto) {
-        alert(`Has acertado, el numero secreto es: ${numeroSecreto}. Lo hiciste en ${intentos} ${intentosPalabra}.`);
+        alert(`Has acertado, el numero secreto es: ${numeroSecreto}. Lo hiciste en ${intentos} ${intentos == 1 ? "intento" : "INTENTOSS"}.`);
     } else {
         if (numeroUsuario < numeroSecreto) {
             alert("El numero secreto es mayor.")
         } else {
             alert("El numero secreto es menor.")
         }    
-        intentos = intentos + 1;
-        intentosPalabra = "INTENTOS"
-        //alert("Lo siento, no acertaste el numero.")
+        //intentos = intentos + 1;
+        intentos++;
+        //intentosPalabra = "INTENTOS"
     }
 }
 
+//alert("Lo siento, no acertaste el numero, ya se te acabaron los intentos.")
 
 // alert("Hola Mundo"); Muestra cuadro de dialogo con el mensaje que se encuentra entre comillas.
 
